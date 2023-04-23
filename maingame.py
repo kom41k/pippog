eb5356bbf9e475447ddac38e8ec6d20a90bdc2fd
kom41k <75855057+kom41k@users.noreply.github.com>
2023-04-23 15:08:44 +0300

    Update maingame.py

--- maingame.py
+++ maingame.py
@@ -31,6 +31,16 @@ class Player(GameSprite):
        if keys[K_s] and self.rect.y < win_height - 80:
            self.rect.y += self.speed
 
+class Bot(GameSprite):
+    def updateup(self):
+        if racket2.rect.centery < 20 + ball.rect.y:
+            self.rect.y += self.speed
+    def updatedown(self):
+        if racket2.rect.centery > 20 + ball.rect.y:
+            self.rect.y -= self.speed
+    def sbros(self):
+        self.rect.y = 185
+
 
 #игровая сцена:
 back = (0, 255, 0) #цвет фона (background)
@@ -46,17 +56,15 @@ finish = False
 clock = time.Clock()
 FPS = 60
 score1 = 0
-score2= 0
+score2 = 0
 #создания мяча и ракетки   
-racket1 = Player('mama.png', 30, 200, 4, 50, 150) 
-racket2 = Player('papa.png', 520, 200, 4, 50, 150)
-ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
+racket1 = Player('Красный.png', 30, 200, 4, 50, 150) 
+racket2 = Player('Синий.png', 520, 200, 4, 50, 150)
+ball = GameSprite('ball.jfif', 200, 200, 4, 50, 50)
 
 
 font.init()
 font = font.Font(None, 35)
-lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
-lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 count1 = font.render('player1 goals:' + str(score1), True, (180, 0, 0))
 count2 = font.render('player2 goals:' + str(score2), True, (180, 0, 0))
 
@@ -65,47 +73,48 @@ speed_y = 3
 
 
 while game:
-   for e in event.get():
-       if e.type == QUIT:
-           game = False
-  
-   if finish != True:
-       window.fill(back)
-       racket1.update_l()
-       racket2.update_r()
-       ball.rect.x += speed_x
-       ball.rect.y += speed_y
-       window.blit(count1, (0, 0))
-       window.blit(count2, (400, 0))
-       count1 = font.render('player1 goals:' + str(score1), True, (180, 0, 0))
-       count2 = font.render('player2 goals:' + str(score2), True, (180, 0, 0))
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        count1 = font.render('player1 goals:' + str(score1), True, (180, 0, 0))
+        count2 = font.render('player2 goals:' + str(score2), True, (180, 0, 0))
 
 
-       if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
-           speed_x *= -1
-           speed_y *= 1
-      
-       #если мяч достигает границ экрана, меняем направление его движения
-       if ball.rect.y > win_height-50 or ball.rect.y < 0:
-           speed_y *= -1
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        #если мяч достигает границ экрана, меняем направление его движения
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
 
 
-       #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
-       if ball.rect.x < 0:
-           score2 = score2 + 1
-           ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
+        #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+        if ball.rect.x < 0:
+            ball.rect.x = win_width/2
+            score2 += 1
+
+            count2 = font.render('player2 goals:' + str(score2), True, (180, 0, 0))
 
 
-       #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
-       if ball.rect.x > win_width:
-           score1 == score1 + 1
-           ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
+        #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+        if ball.rect.x > win_width:
+            ball.rect.x = win_width/2
+            score1 += 1
+            count1 = font.render('player1 goals:' + str(score1), True, (180, 0, 0))
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+        window.blit(count1, (0, 0))
+        window.blit(count2, (400, 0))
 
-
-       racket1.reset()
-       racket2.reset()
-       ball.reset()
-
-
-   display.update()
-   clock.tick(FPS)
+    display.update()
+    clock.tick(FPS)
